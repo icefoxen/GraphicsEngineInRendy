fda93a63851393bbf44a15bcac9711c2fb369360
Simon Heath <icefox@dreamquest.io>
2019-09-13 12:32:26 -0400

    Hopefully actually clear the screen!

--- rendy_examples/src/bin/03_clear.rs
+++ rendy_examples/src/bin/03_clear.rs
@@ -1,10 +1,62 @@
 use env_logger;
+use failure::Error;
 use log;
 use rendy::wsi::winit::{ControlFlow, Event, EventsLoop, WindowBuilder, WindowEvent};
 
-use rendy::{self, command, factory, graph};
+use rendy::hal;
+use rendy::{
+    self, command, factory, graph,
+    graph::render::{PrepareResult, RenderGroup, RenderGroupDesc},
+};
 
 type Backend = rendy::vulkan::Backend;
+
+#[derive(Debug, Clone, Default)]
+struct ClearGroup;
+
+impl RenderGroup<Backend, ()> for ClearGroup {
+    fn prepare(
+        &mut self,
+        _factory: &factory::Factory<Backend>,
+        _queue: command::QueueId,
+        _index: usize,
+        _subpass: hal::pass::Subpass<Backend>,
+        _aux: &(),
+    ) -> PrepareResult {
+        PrepareResult::DrawRecord
+    }
+
+    fn draw_inline(
+        &mut self,
+        _encoder: command::RenderPassEncoder<Backend>,
+        _index: usize,
+        _subpass: hal::pass::Subpass<Backend>,
+        _aux: &(),
+    ) {
+    }
+
+    fn dispose(self: Box<Self>, _factory: &mut factory::Factory<Backend>, _aux: &()) {}
+}
+
+#[derive(Debug, Clone, Default)]
+struct ClearGroupDesc;
+
+impl RenderGroupDesc<Backend, ()> for ClearGroupDesc {
+    fn build(
+        self,
+        _ctx: &graph::GraphContext<Backend>,
+        _factory: &mut factory::Factory<Backend>,
+        _queue: command::QueueId,
+        _aux: &(),
+        _framebuffer_width: u32,
+        _framebuffer_height: u32,
+        _subpass: hal::pass::Subpass<Backend>,
+        _buffers: Vec<graph::NodeBuffer>,
+        _images: Vec<graph::NodeImage>,
+    ) -> Result<Box<dyn RenderGroup<Backend, ()> + 'static>, Error> {
+        Ok(Box::new(ClearGroup))
+    }
+}
 
 fn run(
     mut event_loop: EventsLoop,
@@ -54,22 +106,18 @@ fn main() {
 
     let mut graph_builder = graph::GraphBuilder::<Backend, ()>::new();
 
-    /*
     graph_builder.add_node(
-        TriangleRenderPipeline::builder()
-            .into_subpass()
+        graph::render::SubpassBuilder::new()
             .with_color_surface()
+            .with_group(ClearGroupDesc::default().builder())
             .into_pass()
             .with_surface(
                 surface,
-                Some(hal::command::ClearValue {
-                    color: hal::command::ClearColor {
-                        float32: [1.0, 1.0, 1.0, 1.0],
-                    },
-                }),
+                Some(hal::command::ClearValue::Color(
+                    hal::command::ClearColor::Sfloat([0.1, 0.2, 0.3, 1.0]),
+                )),
             ),
     );
-    */
     let graph = graph_builder
         .build(&mut factory, &mut families, &())
         .unwrap();
